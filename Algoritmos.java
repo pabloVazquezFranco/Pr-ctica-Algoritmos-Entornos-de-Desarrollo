83a8d74e4af65a5ed02f477b0b9dfc5498bebe80
pabloVazquezFranco <pvf0003@alu.medac.es>
2025-05-12 19:59:09 +0200

    Factorial

--- Algoritmos.java
+++ Algoritmos.java
@@ -17,5 +17,22 @@ public abstract class Algoritmos {
         }
         return fibonacci(numero - 1) + fibonacci(numero - 2);
     }
+    
+    /**
+     * Calcula el factorial de un número dado sin el operador ternario.
+     * @param numero Número para calcular el factorial.
+     * @return Factorial del número dado.
+     * @throws IllegalArgumentException Si el número es menor que cero.
+     */
+    public static int factorial(int numero) {
+        if (numero < 0) {
+            throw new IllegalArgumentException("El número debe ser mayor o igual a cero.");
+        }
+        if (numero == 0) {
+            return 1;
+        } else {
+            return numero * factorial(numero - 1);
+        }
+    }
 
 }
